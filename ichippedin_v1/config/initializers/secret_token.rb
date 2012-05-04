04219d7c9dcc8b220b7e5446cb98da04e717871a
Marcus Stewart <stewart.marcus@gmail.com>
2012-05-03 21:50:49 -0500

    first commit

---
+++ ichippedin_v1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IchippedinV1::Application.config.secret_token = '3e2ccb14586ba1eef167a6eae98bb2de60d802578bdd85631a2fec75dea45bd942410dcb856ebe3592a7b1a176a616cd56e772b7028a2c17803f5563e169ae8c'
